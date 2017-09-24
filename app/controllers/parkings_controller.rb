1e2d7e220547ae1ac3353124ab169734ba16e986
jkxruby <jkxruby@gmail.com>
2017-09-24 14:41:49 +0800

    modify the interface of views

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -6,7 +6,15 @@ class ParkingsController < ApplicationController
 
   # step2: 新建一笔停车，并记录开始时间
   def create
-    @parking = Parking.new(parking_type: 'guest', start_at: Time.now)
+    @parking = Parking.new(start_at: Time.now)
+    # 有登陆的话，根据用户设定费用，没有登陆，按过客收费
+    if current_user
+      @parking.parking_type = params[:parking][:parking_type]
+      @parking.user = current_user
+    else
+      @parking.parking_type  = "guest"
+    end
+    
     @parking.save!
     redirect_to parking_path(@parking)
   end
