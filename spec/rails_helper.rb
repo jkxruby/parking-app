146e01b9602a257869b6cce18ba548cb90ca40bf
jkxruby <jkxruby@gmail.com>
2017-09-25 14:08:14 +0800

    test the common-amount pay 4-3

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,8 +55,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.filter_run :focus => true
-  config.run_all_when_everything_filtered = true 
+require 'capybara/rails'
+require 'capybara/rspec'
 
 
 
